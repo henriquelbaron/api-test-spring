4460819056e8e6edca988d7c0fc51f035862ca67
Henrique Baron <hike.lemes@gmail.com>
2020-10-11 16:50:21 -0300

    trocando banco para postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into usuario(login,senha,nome,administrador) values ('convidado', 'manager', 'Usuário convidado', false);
 insert into usuario(login,senha,nome,administrador) values ('admin', 'suporte', 'Gestor', true);
-insert into token(token,login,expiracao,administrador) values ('SDFLHJKLASFKAWEFILUUJFSUESFGHJESJ', 'admin', now () - INTERVAL 10 DAY, true);
 insert into pais(nome,sigla,gentilico) values ('Brasil', 'BR', 'Brasileiro');
 insert into pais(nome,sigla,gentilico) values ('Argentina', 'AR', 'Argentino');
 insert into pais(nome,sigla,gentilico) values ('Alemanha', 'AL', 'Alemão');
+insert into token(token,login,expiracao,administrador) values ('SDFLHJKLASFKAWEFILUUJFSUESFGHJESJ', 'admin', now () - INTERVAL 10 DAY, true);
 
 
 --insert into usuario(id,login,senha,nome,administrador) values (seq_usuario.NEXTVAL, 'convidado', 'manager', 'Usuário convidado', false);
