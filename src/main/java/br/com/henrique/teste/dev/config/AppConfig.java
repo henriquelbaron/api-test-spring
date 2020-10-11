58b42faea7be9d23e38bc810d8d9b89dfe0caf12
Henrique Baron <hike.lemes@gmail.com>
2020-10-11 16:04:25 -0300

    config swagger

---
+++ src/main/java/br/com/henrique/teste/dev/config/AppConfig.java
@@ -0,0 +1,15 @@
+package br.com.henrique.teste.dev.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
