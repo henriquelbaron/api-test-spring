db39cb74e40e1c203b1bc0cf4e484446ae5ca900
Henrique Baron <hike.lemes@gmail.com>
2020-10-11 15:49:33 -0300

    config swagger

--- src/main/java/br/com/henrique/teste/dev/config/SwaggerConfig.java
+++ src/main/java/br/com/henrique/teste/dev/config/SwaggerConfig.java
@@ -9,11 +9,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Classe responsavel por concentrar configuracao referente ao Swagger
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
