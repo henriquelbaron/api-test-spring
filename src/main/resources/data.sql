b50a08c3528b63aba5a2601282516e62f15219f3
Henrique Baron <hike.lemes@gmail.com>
2020-10-11 16:30:11 -0300

    config swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into usuario(id,login,senha,nome,administrador) values (SEQ_USUARIO.NEXTVAL, 'convidado', 'manager', 'Usuário convidado', false);
-insert into usuario(id,login,senha,nome,administrador) values (SEQ_USUARIO.NEXTVAL, 'admin', 'suporte', 'Gestor', true);
-insert into token(id,token,login,expiracao,administrador) values (SEQ_TOKEN.NEXTVAL, 'SDFLHJKLASFKAWEFILUUJFSUESFGHJESJ', 'admin', now () - INTERVAL 10 DAY, true);
-insert into pais(id,nome,sigla,gentilico) values (SEQ_PAIS.NEXTVAL, 'Brasil', 'BR', 'Brasileiro');
-insert into pais(id,nome,sigla,gentilico) values (SEQ_PAIS.NEXTVAL, 'Argentina', 'AR', 'Argentino');
-insert into pais(id,nome,sigla,gentilico) values (SEQ_PAIS.NEXTVAL, 'Alemanha', 'AL', 'Alemão');
+insert into usuario(id,login,senha,nome,administrador) values (seq_usuario.NEXTVAL, 'convidado', 'manager', 'Usuário convidado', false);
+insert into usuario(id,login,senha,nome,administrador) values (seq_usuario.NEXTVAL, 'admin', 'suporte', 'Gestor', true);
+insert into token(id,token,login,expiracao,administrador) values (seq_token.NEXTVAL, 'SDFLHJKLASFKAWEFILUUJFSUESFGHJESJ', 'admin', now () - INTERVAL 10 DAY, true);
+insert into pais(id,nome,sigla,gentilico) values (seq_pais.NEXTVAL, 'Brasil', 'BR', 'Brasileiro');
+insert into pais(id,nome,sigla,gentilico) values (seq_pais.NEXTVAL, 'Argentina', 'AR', 'Argentino');
+insert into pais(id,nome,sigla,gentilico) values (seq_pais.NEXTVAL, 'Alemanha', 'AL', 'Alemão');
